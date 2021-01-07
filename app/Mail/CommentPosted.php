28909ed9938b1012520eb3e548d292cb65e8891d
RyutoYorisato <ryutoyorisato@RyutoYorisatonoMacBook-Pro.local>
2021-01-07 20:07:11 +0900

    20210107

--- app/Mail/CommentPosted.php
+++ app/Mail/CommentPosted.php
@@ -2,32 +2,30 @@
 
 namespace App\Mail;
 
+use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
 class CommentPosted extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $user;
+    public $comment;
+
+    public function __construct(User $user, Comment $comment)
     {
-        //
+        $this->user = $user;
+        $this->comment = $comment;
     }
 
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
     public function build()
     {
-        return $this->view('view.name');
+        return $this
+            ->subject('コメントありがとうございます')
+            ->view('emails.comments.posted');
     }
 }
--- resources/lang/jp/passwords.php
+++ resources/lang/jp/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'パスワードを再設定しました。',
+    'sent' => 'パスワード再設定リンクを送信しました。',
+    'throttled' => '再設定するまでの時間を空けてください。',
+    'token' => 'トークンが無効です。',
+    'user' => "入力されたメールアドレスのユーザーは見つかりませんでした。",
 
 ];
